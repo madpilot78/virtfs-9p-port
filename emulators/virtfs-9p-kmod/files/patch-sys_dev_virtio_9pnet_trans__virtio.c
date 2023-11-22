f5a5479d5447aa36bb056170b3afac0a31f645bf
Guido Falsi <mad@madpilot.net>
2023-11-22 09:12:29 +0100

    Add patch to fix build on recent head.
    Fix needed after https://github.com/freebsd/freebsd-src/commit/180c02405b12e08bbb69bb693a38077db0c85e9b

--- emulators/virtfs-9p-kmod/files/patch-sys_dev_virtio_9pnet_trans__virtio.c
+++ emulators/virtfs-9p-kmod/files/patch-sys_dev_virtio_9pnet_trans__virtio.c
@@ -1,6 +1,18 @@
 --- sys/dev/virtio/9pnet/trans_virtio.c.orig	2022-03-23 13:18:44 UTC
 +++ sys/dev/virtio/9pnet/trans_virtio.c
-@@ -502,7 +502,12 @@ vt9p_modevent(module_t mod, int type, void *unused)
+@@ -240,7 +240,11 @@ vt9p_alloc_virtqueue(struct vt9p_softc *sc)
+ 	    vt9p_intr_complete, sc, &sc->vt9p_vq,
+ 	    "%s request", device_get_nameunit(dev));
+ 
++#if __FreeBSD_version >= 1500001
++	return (virtio_alloc_virtqueues(dev, 1, &vq_info));
++#else
+ 	return (virtio_alloc_virtqueues(dev, 0, 1, &vq_info));
++#endif
+ }
+ 
+ /* Probe for existence of 9P virtio channels */
+@@ -502,7 +506,12 @@ vt9p_modevent(module_t mod, int type, void *unused)
  	return (error);
  }
  
